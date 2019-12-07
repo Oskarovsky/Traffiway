81c47895246775720dd8000bb68c48f6e0ff3869
oskarro <oskar.slyk@gmail.com>
2019-12-07 21:46:45 +0100

    Error handler

--- app/app.py
+++ app/app.py
@@ -7,10 +7,6 @@ bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
 @app.route('/index')
 def index():
     return render_template('index.html')
@@ -19,5 +15,15 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
