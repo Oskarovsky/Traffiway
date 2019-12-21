a243cf396141c1a24cd9e98eea309ce7a72505b7
oskarro <oskar.slyk@gmail.com>
2019-12-21 16:01:37 +0100

    register fix

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,7 +7,7 @@ from ..models import User
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
-    username = StringField('Username', validators=[DataRequired, Length(1, 64),
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64),
                                                    Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                                           'Usernames must have only letters, numbers and dots.')])
     password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Password must match.')])
