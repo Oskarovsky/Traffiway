bd21e3aa9845485a6e720f03fa55ed5185247789
oskarro <oskar.slyk@gmail.com>
2019-12-26 21:29:26 +0100

    role assignment

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from flask import current_app
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
@@ -29,6 +29,28 @@ class Role(db.Model):
     def has_permission(self, perm):
         return self.permissions & perm == perm
 
+    # function which is trying find existing role by name and update those
+    # new role object is creating only for roles that aren't in the database already
+    @staticmethod
+    def insert_roles():
+        # TODO READ TEXT BELOW THAT CODE INTO PDF
+        roles = {
+            'Guest': [Permission.FIND],
+            'User': [Permission.FIND, Permission.OPTIMIZE],
+            'Admin': [Permission.FIND, Permission.OPTIMIZE, Permission.ADMIN]
+        }
+        default_role='User'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
     def __init__(self, **kwargs):
         super(Role, self).__init__(**kwargs)
         if self.permissions is None:
@@ -36,6 +58,12 @@ class Role(db.Model):
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+
+class Permission:
+    FIND = 1
+    OPTIMIZE = 2
+    ADMIN = 4
 
 
 class User(UserMixin, db.Model):
@@ -114,8 +142,35 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    # evaluating whether a user has a given permission
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+
     def __repr__(self):
         return '<User %r>' % self.username
+
+    # User constructor
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['TRAFFIWAY_ADMIN']:
+                self.role = Role.query.filter_by(name='Administrator').first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+
+login_manager.anonymous_user = AnonymousUser
 
 
 @login_manager.user_loader
